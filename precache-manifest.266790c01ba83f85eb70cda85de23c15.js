a2fab29a827d81e9158eadc68e5236a6e6a585b2
LucasHdS <lucasnsv@gmail.com>
2019-11-27 14:11:42 -0300

    Updates

---
+++ precache-manifest.266790c01ba83f85eb70cda85de23c15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e68fd4339a557d59eeea83593870153",
+    "url": "/SchoolClient/index.html"
+  },
+  {
+    "revision": "29f8250bf86a06ab73cf",
+    "url": "/SchoolClient/static/css/main.a7589efe.chunk.css"
+  },
+  {
+    "revision": "54e2fdebe09b89bef32a",
+    "url": "/SchoolClient/static/js/2.f669a876.chunk.js"
+  },
+  {
+    "revision": "29f8250bf86a06ab73cf",
+    "url": "/SchoolClient/static/js/main.f2cebcb8.chunk.js"
+  },
+  {
+    "revision": "deb3f0c68eb5cf45f5cc",
+    "url": "/SchoolClient/static/js/runtime-main.24efdf7b.js"
+  }
+]);
